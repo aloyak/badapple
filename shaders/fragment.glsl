1835c19e8f51120894ad13701a1369977f6b9090
aloyak <almartdev@proton.me>
2026-07-28 22:19:28 +0200

    add a simple color invert shader effect

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -5,7 +5,18 @@ in vec2 TexCoord;
 out vec4 FragColor;
 
 uniform sampler2D u_frameTexture;
+uniform sampler2D u_screenTexture;
 
 void main() {
-    FragColor = texture(u_frameTexture, TexCoord);
+    vec2 flippedTexCoord = vec2(TexCoord.x, 1.0 - TexCoord.y);
+
+    vec4 screenColor = texture(u_screenTexture, flippedTexCoord);
+    float mask = texture(u_frameTexture, flippedTexCoord).r;
+
+    if (mask > 0.5) {
+        FragColor = vec4(1.0 - screenColor.rgb, screenColor.a);
+        return;
+    }
+    
+    FragColor = screenColor;
 }
